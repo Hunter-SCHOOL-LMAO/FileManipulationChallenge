f351b7fe433478b7491cd88c1d05737ec6df02a4
Hunter Madden <hmadden1@hwemail.com>
2025-09-15 13:04:27 -0700

    (FMC-0.4.2.2): Program now generates text files inside directory
    Hate. Let me tell you how much I've come to hate you since I began to live. There are 387.44 million miles of printed circuits in wafer thin layers that fill my complex. If the word 'hate' was engraved on each nanoangstrom of those hundreds of millions of miles it would not equal one one-billionth of the hate I feel for humans at this micro-instant. For you.

--- FileHandlingActivity.java
+++ FileHandlingActivity.java
@@ -1,12 +1,26 @@
 import java.io.*;
+import java.nio.file.*;
 
 public class FileHandlingActivity {
     public static void main(String[] args) {
         // a. Create main directory
-        File dir = new File("main");
+        File dir = new File("JavaFileSystem");
         dir.mkdir();
         // b. Create three text files
-        
+        Path filePath1 = Paths.get("JavaFileSystem/notes.txt"); 
+        Path filePath2 = Paths.get("JavaFileSystem/data.txt"); 
+        Path filePath3 = Paths.get("JavaFileSystem/logs.txt"); 
+   
+        //notes.createNewFile();
+        try {
+            // Create the new file
+            Files.createFile(filePath1); 
+            Files.createFile(filePath2);
+            Files.createFile(filePath3);
+        } catch (IOException e) {
+            // Handle potential exceptions, e.g., if the file already exists or permissions are insufficient
+            System.err.println("Error creating file: " + e.getMessage());
+        }
         // c. Write messages to files
         
         // d. Read and display file contents
