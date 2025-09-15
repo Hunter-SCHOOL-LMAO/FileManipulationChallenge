8e5648e08dc22c70dd5ea9e8320d3c2fa05f09f3
Hunter Madden <hmadden1@hwemail.com>
2025-09-15 12:27:34 -0700

    (FMC-0.4.2.1): Program can make a directory
    Adds code to create a directory

---
+++ FileHandlingActivity.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class FileHandlingActivity {
+    public static void main(String[] args) {
+        // a. Create main directory
+        File dir = new File("main");
+        dir.mkdir();
+        // b. Create three text files
+        
+        // c. Write messages to files
+        
+        // d. Read and display file contents
+        
+        // e. Create backup directory
+        
+        // f. Copy contents to backup file
+        
+        // g. List all files in both directories
+    }
+}
